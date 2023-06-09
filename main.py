d074afba6ddae1da9a0b33aca938c257b469a6af
emrahaydemir23 <116636065+emrahaydemir23@users.noreply.github.com>
2023-06-09 14:54:50 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+#st.markdown("www.google.com", unsafe_allow_html=True)
+
+st.set_page_config(page_title="HAKTAM", layout="wide", page_icon=":+1:") # https://www.webfx.com/tools/emoji-cheat-sheet/
+st.title("HAKTAM")
