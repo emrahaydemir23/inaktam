9a6fb69dd65f5fd3f0274633723d415dd1bffa03
emrahaydemir23 <116636065+emrahaydemir23@users.noreply.github.com>
2023-10-03 14:42:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ parametre = st.experimental_get_query_params()
 degerler = parametre["d"][0]
 model = parametre["m"][0]
 
-tahminSonucu = predict(model, degerler)
-st.title("Tahmin Sonucu:  "+str(tahminSonucu))
+#tahminSonucu = predict(model, degerler)
+#st.title("Tahmin Sonucu:  "+str(tahminSonucu))
+st.title("Tahmin Sonucu:  "+str(model)+"\n\n"+str(degerler))
 
 
 st.markdown('[Geri Git](https://inaktam.erciyes.edu.tr/kararTahmini.aspx)')
